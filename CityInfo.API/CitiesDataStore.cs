74841e9ef742b2f7ac0232bd8b3d91caecc97450
Darius Dobocan <darius.dobocan@lateralgroup.com>
2023-07-28 13:34:48 +0300

    Added points of interest

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -16,19 +16,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one without the towers."
+                    Description = "The one without the towers.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "Most visited park in th US."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Not the towers",
+                            Description = "Probably an inside job."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id= 2,
                     Name = "Craiova",
-                    Description = "The one with the knives."
+                    Description = "The one with the knives.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Central Park",
+                            Description = "Most visited park in th Romania(probably not)."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "London",
-                    Description = "God Save The King."
+                    Description = "God Save The King.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "test1",
+                            Description = "123"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "test2",
+                            Description = "123."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "test3",
+                            Description = "123"
+                        }
+                    }
                 }
             };
         }
